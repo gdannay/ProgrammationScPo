ee520ea814bb047b7a78c614142d1bed31ea95f1
Tyler Abbot <tyler.abbot@sciencespo.fr>
2016-09-26 12:48:28 +0200

    Tylers comments on weeks 1-3.

--- Session3_HW/homework3.R
+++ Session3_HW/homework3.R
@@ -1,7 +1,7 @@
 # QUESTION 1
 
 #' Add all numbers of a common vector
-#' 
+#'
 #' @author Tyler Abbot
 #' @param x vector: a vector with a serie of number
 #' @return The sum of all nubers included in the vector
@@ -17,6 +17,10 @@ our_sum <- function(x){
 
 
 # QUESTION 2
+
+############# Comment
+# Watch out! This is taking the inverse F to be one thing, but the random unif
+# to be on (0, 10). This probably gave you wierd answers!
 
 rand_sample <- function(n, F_inv=function(x) x^2){
   unif_sample <- runif(n, 0, 10)
