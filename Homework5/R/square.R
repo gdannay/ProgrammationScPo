09d982d24aa2cb0642813984e494f978fcb1267b
gdannay <gregory.dannay@sciencespo.fr>
2016-10-07 00:34:53 +0200

    Homework 5

---
+++ Homework5/R/square.R
@@ -0,0 +1,11 @@
+#' Square of a number
+#'
+#' @param number double: a number given by the user
+#' @return The square of this number
+#' @export
+#' @examples
+#' square(3)
+
+square <- function(x){
+  return(x*x)
+}
